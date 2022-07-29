dd3b9e1b1696904d6b75f796fc9719b862a98f8e
liangfeng_wang <liangfeng_w@kingdee.com>
2022-07-29 10:12:39 +0800

    fix: [drawer] 修复纵向抽屉无法设置高度问题

--- components/drawer/drawer.tsx
+++ components/drawer/drawer.tsx
@@ -259,6 +259,7 @@ const InternalDrawer = (props: IDrawerProps, ref: unknown): ReactElement | React
           transform: transform,
         }
       : {
+          height,
           transform: transform,
         },
   )
