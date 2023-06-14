a6ec20b8f635a4d0739496a3e14ad9b5ddd5a5f4
oujj <287068328@qq.com>
2023-06-14 16:23:46 +0800

    fix: icon single test modifications

--- components/icon/__tests__/index.test.tsx
+++ components/icon/__tests__/index.test.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { mount, render } from 'enzyme'
 import Icon from '../index'
+import { IconTypes } from '../interface'
+import ConfigProvider from '../../config-provider/index'
+
 import matchMediaPolyfill from 'mq-polyfill'
+import mountTest from '../../../tests/shared/mountTest'
 
 beforeAll(() => {
   matchMediaPolyfill(window)
@@ -16,34 +20,97 @@ beforeAll(() => {
 })
 
 describe('Icon', () => {
-  it('should render Icon', () => {
-    const wrapper = render(<Icon type="add" />)
-    expect(wrapper).toMatchSnapshot()
+  // 1. mount test
+  describe('1. mount test', () => {
+    mountTest(Icon)
+    IconTypes.forEach((type) => {
+      mountTest(() => <Icon type={type} />)
+    })
   })
 
-  it('when className is kingdee', () => {
-    const wrapper = mount(<Icon type="add" className="kingdee" />)
-    console.log('wrapper', wrapper.find('i').first())
-    expect(wrapper.find('i').first()).toHaveClassName('kingdee')
+  // 2.render test
+  describe('2. render test', () => {
+    it('should render Icon', () => {
+      const wrapper = render(<Icon type="add" />)
+      expect(wrapper).toMatchSnapshot()
+    })
   })
 
-  it('when prefix is kd', () => {
-    const wrapper = mount(<Icon type="add" prefix="kd" />)
-    expect(wrapper.find('i').first()).toHaveClassName('kd-add')
+  // 3.warns in component
+
+  // 4. render null or undefined without errors
+  describe('4. render null or undefined without errors', () => {
+    it('render null or undefined without errors', () => {
+      expect(
+        mount(
+          <Icon type="add">
+            {null}
+            {undefined}
+          </Icon>,
+        ).find('.kdicon'),
+      ).toMatchSnapshot()
+    })
   })
 
-  it('when rotate is 90', () => {
-    const wrapper = mount(<Icon type="add" rotate={90} />)
-    expect(wrapper.find('i').first()).toHaveStyle({ transform: 'rotate(90deg)' })
+  // 5. displayName
+  it('should have displayName static property', () => {
+    expect(Icon.displayName).toBe('Icon')
   })
 
-  it('when spin	is true ', () => {
-    const wrapper = mount(<Icon type="add" spin={true} />)
-    expect(wrapper.find('i').first()).toHaveClassName('icon-spin')
+  // 6. api test
+  describe('api test', () => {
+    it('when className is kingdee', () => {
+      const wrapper = mount(<Icon type="add" className="kingdee" />)
+      expect(wrapper.find('i').first()).toHaveClassName('kingdee')
+    })
+
+    it('when prefix is kd', () => {
+      const wrapper = mount(<Icon type="add" prefix="kd" />)
+      expect(wrapper.find('i').first()).toHaveClassName('kd-add')
+    })
+
+    it('when rotate is 90', () => {
+      const wrapper = mount(<Icon type="add" rotate={90} />)
+      expect(wrapper.find('i').first()).toHaveStyle({ transform: 'rotate(90deg)' })
+    })
+
+    it('when spin	is true', () => {
+      const wrapper = mount(<Icon type="add" spin={true} />)
+      expect(wrapper.find('i').first()).toHaveClassName('icon-spin')
+    })
+
+    it('when style is color:red', () => {
+      const wrapper = mount(<Icon type="add" style={{ color: 'red' }} />)
+      expect(wrapper.find('i').first()).toHaveStyle({ color: 'red' })
+    })
   })
 
-  it('when style is color:red ', () => {
-    const wrapper = mount(<Icon type="add" style={{ color: 'red' }} />)
-    expect(wrapper.find('i').first()).toHaveStyle({ color: 'red' })
+  // 7.component interaction(event)
+
+  // 8.config provider
+  describe('8.config provider', () => {
+    it('should config use config provider', () => {
+      const Config = {
+        compDefaultProps: {
+          Icon: {
+            prefix: 'kingdeeicon',
+          },
+        },
+      }
+      const wrapper = mount(
+        <ConfigProvider value={Config}>
+          <Icon type="add" />
+        </ConfigProvider>,
+      )
+      // console.log(wrapper.debug())
+      expect(wrapper.find('.kingdeeicon-add')).toExist()
+    })
   })
+
+  // 9. ref test
+  // it('should get Icon element from ref', () => {
+  //   const ref = React.createRef()
+  //   mount(<Icon ref={ref} type="add"></Icon>)
+  //   expect(ref.current instanceof HTMLSpanElement).toBe(true)
+  // })
 })
