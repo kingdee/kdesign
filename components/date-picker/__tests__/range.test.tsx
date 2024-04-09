1f2b3a8e001c78056fce6703bbe614a4c5f4c2bf
hxh2010 <hongxh.work@gmail.com>
2024-04-09 10:19:12 +0800

    fix: [date-picker]修复null值问题 fix #760

--- components/date-picker/__tests__/range.test.tsx
+++ components/date-picker/__tests__/range.test.tsx
@@ -557,4 +557,25 @@ describe('RangePicker', () => {
       expect(wrapper.find(selector).at(index).prop('style')).toEqual({ background: 'red' })
     })
   })
+
+  // 18. null with popup
+  it('null with popup', async () => {
+    const ref: any = React.createRef()
+    const onChange = jest.fn()
+    const wrapper = mount(
+      <div ref={ref}>
+        <RangePicker
+          value={[new Date(START_DAY), null]}
+          defaultOpen
+          onChange={onChange}
+          getPopupContainer={() => ref.current}
+        />
+      </div>,
+    )
+
+    wrapper.find('input').at(0).simulate('focus')
+    wrapper.find('.kd-date-picker-calendar-text').at(0).simulate('click')
+    wrapper.find('.kd-date-picker-calendar-text').at(1).simulate('click')
+    expect(onChange).toHaveBeenCalledTimes(1)
+  })
 })
