165dc14969b59b274662308958f29e16a2a8a374
hxh2010 <hongxh.work@gmail.com>
2024-11-25 16:28:28 +0800

    fix: [input]typescript类型报错 fix #1009

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,6 +1,7 @@
 import Input from './input'
 import TextArea from './TextArea'
-export { InputProps, InputSiteTypes, InputSiteType, BorderTypes, BorderType } from './input'
-// export { TextAreaProps } from './TextArea'
+export { InputSiteTypes, BorderTypes } from './input'
+export type { InputProps, InputSiteType, BorderType } from './input'
+export type { TextAreaProps } from './TextArea'
 export { TextArea }
 export default Input
