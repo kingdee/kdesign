a81c332a334575bf71f71e1263d7138f6f9894bb
zcy97 <1050365185@qq.com>
2025-07-18 10:17:45 +0800

    feat: [table]添加样式前缀

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -157,6 +157,7 @@ const Table = forwardRef<unknown, TableProps>((props: TableProps, ref) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       isMobile={isMobile}
+      prefixCls={prefixCls}
       {...others}
     />
   )
